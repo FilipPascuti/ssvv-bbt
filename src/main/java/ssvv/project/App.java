0a91c34487c4b96cd52797fafb66a10052df5b9b
Pascuti Filip <fspascuti@gmail.com>
2022-03-18 09:42:57 +0200

    initial project

---
+++ src/main/java/ssvv/project/App.java
@@ -0,0 +1,7 @@
+package ssvv.project;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ssvv/project/Main.java
@@ -0,0 +1,36 @@
+package ssvv.project;
+
+
+import ssvv.project.console.UI;
+import ssvv.project.domain.Nota;
+import ssvv.project.domain.Student;
+import ssvv.project.domain.Tema;
+import ssvv.project.repository.NotaXMLRepository;
+import ssvv.project.repository.StudentXMLRepository;
+import ssvv.project.repository.TemaXMLRepository;
+import ssvv.project.service.Service;
+import ssvv.project.validation.NotaValidator;
+import ssvv.project.validation.StudentValidator;
+import ssvv.project.validation.TemaValidator;
+import ssvv.project.validation.Validator;
+
+public class Main {
+    public static void main(String[] args) {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        UI consola = new UI(service);
+        consola.run();
+
+        //PENTRU GUI
+        // de avut un check: daca profesorul introduce sau nu saptamana la timp
+        // daca se introduce nota la timp, se preia saptamana din sistem
+        // altfel, se introduce de la tastatura
+    }
+}
